5f66d30be845ada20e2fe89221adc281e924f069
OlafFilies <olaf.filies@t-online.de>
2025-06-03 00:21:15 +0200

    clean up simple magnetometer sketch

--- examples/PSoC_SimpleMagnetometer/PSoC_SimpleMagnetometer.ino
+++ examples/PSoC_SimpleMagnetometer/PSoC_SimpleMagnetometer.ino
@@ -1,44 +1,42 @@
-/*
-  Arduino BMM150 - Simple Magnetometer
-
-  This example reads the magnetic field values from the BMM150
-  sensor and continuously prints them to the Serial Monitor
-  or Serial Plotter.
-
-  The circuit:
-  - Arduino Nano 33 BLE Sense Rev2
-
-  created 10 Jul 2019
-  by Riccardo Rizzo
-
-  This example code is in the public domain.
-*/
+/**
+ * @file PSoC_SimpleMagnetometer.ino
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 
 #include "BoschSensorClass_PSoC.hpp"
+
+// Switch to 1 if you also want to collect the sensor temperature
+#define WITH_TEMPERATURE  0
+
 
 void setup() {
     Serial.begin(115200);
     while (!Serial);
     Serial.println("Started");
 
-    /* the sample rate can be set to
-    * 400Hz                  |  BMM350_DATA_RATE_400HZ (default)
-    * 200Hz                  |  BMM350_DATA_RATE_200HZ
-    * 100Hz                  |  BMM350_DATA_RATE_100HZ
-    * 50Hz                   |  BMM350_DATA_RATE_50HZ
-    * 25Hz                   |  BMM350_DATA_RATE_25HZ
-    * 12.5Hz                 |  BMM350_DATA_RATE_12_5HZ 
-    * 6.25Hz                 |  BMM350_DATA_RATE_6_25HZ
-    * 3.125Hz                |  BMM350_DATA_RATE_3_125HZ
-    * 1.5625Hz               |  BMM350_DATA_RATE_1_5625HZ
-    *
-    * low power/highest noise    |  BMM350_NO_AVERAGING  BMM350_LOWPOWER
-    * lesser noise               |  BMM350_AVERAGING_2   BMM350_REGULARPOWER
-    * even lesser noise          |  BMM350_AVERAGING_4   BMM350_LOWNOISE
-    * lowest noise/highest power |  BMM350_AVERAGING_8   BMM350_ULTRALOWNOISE
-
-    */
-    IMU_PSoC.magneticSensorPreset(BMM350_DATA_RATE_25HZ, BMM350_ULTRALOWNOISE);
+    /** the sample rate can be set to
+     * 400Hz                  |  BMM350_DATA_RATE_400HZ (default)
+     * 200Hz                  |  BMM350_DATA_RATE_200HZ
+     * 100Hz                  |  BMM350_DATA_RATE_100HZ
+     * 50Hz                   |  BMM350_DATA_RATE_50HZ
+     * 25Hz                   |  BMM350_DATA_RATE_25HZ
+     * 12.5Hz                 |  BMM350_DATA_RATE_12_5HZ 
+     * 6.25Hz                 |  BMM350_DATA_RATE_6_25HZ
+     * 3.125Hz                |  BMM350_DATA_RATE_3_125HZ
+     * 1.5625Hz               |  BMM350_DATA_RATE_1_5625HZ
+     *
+     * low power/highest noise    |  BMM350_NO_AVERAGING  BMM350_LOWPOWER
+     * lesser noise               |  BMM350_AVERAGING_2   BMM350_REGULARPOWER
+     * even lesser noise          |  BMM350_AVERAGING_4   BMM350_LOWNOISE
+     * lowest noise/highest power |  BMM350_AVERAGING_8   BMM350_ULTRALOWNOISE
+     */
+    IMU_PSoC.magneticSensorPreset(BMM350_DATA_RATE_12_5HZ, BMM350_AVERAGING_2);
 
     /**
      *                powermode |   Power mode
@@ -50,12 +48,16 @@ void setup() {
      */
     IMU_PSoC.magneticPowerMode(BMM350_NORMAL_MODE);
 
+    /**
+     * Enable or disable x,y,z axes, be default all three are enabled
+     */
+    IMU_PSoC.magneticEnableAxes();
+
     // To configure the BMM3500
     if (!IMU_PSoC.begin(BOSCH_MAGNETOMETER_ONLY)) {
         Serial.println("Failed to initialize IMU_PSoC!");
         while (1);
     }
-
 
     Serial.println();
     Serial.println("Magnetic Field in uT");
@@ -72,8 +74,12 @@ void loop() {
     Serial.print(y);
     Serial.print('\t');
     Serial.print(z);
-    Serial.print('\t');
-    Serial.println(t);
+    if (WITH_TEMPERATURE)
+    {
+      Serial.print('\t');
+      Serial.print(t);
+    }
+    Serial.println("");
 
     delay(10);
 }
